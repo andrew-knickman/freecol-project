9d9c3f968c45deaea9dff904fed00955ff256f32
Adam Lynch <alynch14@students.towson.edu>
2019-05-02 19:00:21 -0400

    Finished setting up the setup method and first test case. Added it to the AllTests suite in common.model

--- test/src/net/sf/freecol/common/model/UnitWasTest.java
+++ test/src/net/sf/freecol/common/model/UnitWasTest.java
@@ -1,16 +1,39 @@
 package net.sf.freecol.common.model;
 
+import net.sf.freecol.util.test.FreeColTestCase;
 
 public class UnitWasTest extends FreeColTestCase{
 	private Unit myUnit;
 	private Game testGame;
 	private String unitID;
+	private UnitWas unitWas;
+	private Specification mySpecs;
+	private UnitType myType;
+	private Role myRole;
+	private static final int ROLE_COUNT = 1;
+	private static final int MOVES_LEFT = 2;
 	
 	@Override
-	public void setUp() {
-		testGame = new Game();
-		unitID = "";
-		myUnit = new Unit(testGame, unitID);
+	protected void setUp() throws Exception{
+		testGame = getGame();
+		unitID = "unit";
+		mySpecs = new Specification();
+		myType = new UnitType(unitID, mySpecs);
+		myUnit = new Unit(testGame);
+		myRole = new Role(unitID, mySpecs);
+		myUnit.setId(unitID);
+		myUnit.setType(myType);
+		myUnit.setRole(myRole);
+		myUnit.setRoleCount(ROLE_COUNT);
+		myUnit.setWorkType(new GoodsType(unitID, mySpecs));
+		myUnit.setMovesLeft(MOVES_LEFT);
 	}
 
+	public void testConstructor() {
+		unitWas = new UnitWas(myUnit);
+		
+		assertEquals(myUnit.getId(), unitID);
+		assertEquals(myUnit.getType(), myType);
+		assertEquals(myUnit.getRole(), myRole);
+	}
 }
